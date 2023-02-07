3d4fd073cd941d730f3bfcd6ff6a10e2b80acd12
Zumori <BohdanCh.w@gmail.com>
2023-02-07 22:47:05 +0200

    Add clearing local storage

--- public_html/js/theme.js
+++ public_html/js/theme.js
@@ -7,10 +7,43 @@ const theme = {
         localStorage.setItem('dark-mode-set', darkModeSet);
     },
     init: function () {
+        theme.drop_local_storage()
         if (theme.isDark()) {
             document.body.classList.add('dark-mode')
         }
-    }
+    },
+
+    drop_local_storage: function () {
+        const version = 0.1
+        const storageKey = 'storage-key-version'
+
+        let currentVersion = JSON.parse(localStorage.getItem(storageKey))
+        if (currentVersion === undefined || currentVersion === null) {
+            currentVersion = 0.0
+        }
+
+        if (currentVersion >= version) {
+            return
+        }
+
+        const shouldKeep = [
+            'dark-mode-set',
+        ]
+
+        const temp = {}
+        for (const i in shouldKeep) {
+            temp[shouldKeep[i]] = localStorage.getItem(shouldKeep[i])
+        }
+
+        localStorage.clear()
+
+        for (const key in temp) {
+            localStorage.setItem(key, temp[key])
+        }
+
+        localStorage.setItem(storageKey, JSON.stringify(version))
+        console.log('local_storage cleared. set version', version)
+    },
 }
 
 document.addEventListener('DOMContentLoaded', theme.init)
